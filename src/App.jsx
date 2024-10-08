241a3d02553e21962913972a9b76cd2812f29b6a
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-08 19:13:25 +0600

    feat(props): pass value by using props

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@
 */
 import { useState } from "react";
 
-function Square() {
+function Square({ value }) {
   return (
     <button
       className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
     >
+      {value}
     </button>
   );
 }
@@ -21,19 +22,19 @@ export default function Board() {
   return (
     <>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value="1"></Square>
+        <Square value="2"></Square>
+        <Square value="3"></Square>
       </div>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value="4"></Square>
+        <Square value="5"></Square>
+        <Square value="6"></Square>
       </div>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value="7"></Square>
+        <Square value="8"></Square>
+        <Square value="9"></Square>
       </div>
     </>
   );
