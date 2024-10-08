3b2c6a106a0e7454687aad4847e3ebc685c46a1b
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-08 23:27:33 +0600

    feat(xIsNext): set xIsNext when click button to toggle x to 0

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,17 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
   const handleClick = (i) => {
     //clone or duplicate square values
     const nextSquare = squares.slice();
-    nextSquare[i] = "X";
+    if (xIsNext) {
+      nextSquare[i] = 'X';
+    } else {
+      nextSquare[i] = 'O';
+    }
     setSquares(nextSquare);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
