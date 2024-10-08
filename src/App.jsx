70cc39dfa229e283cd6b5de2d300a669ba69140d
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-09 01:30:29 +0600

    feat(status): set status as a winner

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,15 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
   const handleClick = (i) => {
     if (squares[i]) {
       return;
@@ -40,17 +49,18 @@ export default function Board() {
   }
   return (
     <>
-      <div>
+      <div>{status}</div>
+      <div className="flex">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
         <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
       </div>
-      <div>
+      <div className="flex">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
         <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
         <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
       </div>
-      <div>
+      <div className="flex">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
         <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
