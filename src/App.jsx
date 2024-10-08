35e58fa0948748b2362b177791c05930a19bf26e
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-08 23:15:46 +0600

    feat(indexValue): pass index valu with handleClick function when click button

--- src/App.jsx
+++ src/App.jsx
@@ -23,28 +23,28 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const handleClick = () => {
+  const handleClick = (i) => {
     //clone or duplicate square values
     const nextSquare = squares.slice();
-    nextSquare[0] = "X";
+    nextSquare[i] = "X";
     setSquares(nextSquare);
   }
   return (
     <>
       <div>
-        <Square value={squares[0]} onSquareClick={handleClick}></Square>
-        <Square value={squares[1]}></Square>
-        <Square value={squares[2]}></Square>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
       </div>
       <div>
-        <Square value={squares[3]}></Square>
-        <Square value={squares[4]}></Square>
-        <Square value={squares[5]}></Square>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
       </div>
       <div>
-        <Square value={squares[6]}></Square>
-        <Square value={squares[7]}></Square>
-        <Square value={squares[8]}></Square>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
       </div>
     </>
   );
