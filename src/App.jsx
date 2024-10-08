fe30e7de2777d34ec9d7cb837c8c165e90905a70
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-08 21:24:19 +0600

    feat(squares): create squares state for every squre component

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,8 @@
 */
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({ value }) {
   const handleClick = () => {
-    setValue("X")
   }
   return (
     <button
@@ -24,22 +22,23 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div>
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[0]}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
       </div>
       <div>
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
       </div>
       <div>
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
       </div>
     </>
   );
