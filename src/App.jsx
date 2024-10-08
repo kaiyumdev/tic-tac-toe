d056462c5efe4f2d6f5e31173da4524cbfd1d8bf
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-08 21:40:05 +0600

    feat(squareClick): add onClick button to work implementing state

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@
 */
 import { useState } from "react";
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   const handleClick = () => {
   }
   return (
     <button
       className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -23,10 +23,13 @@ function Square({ value }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = () => {
+    console.log('click')
+  }
   return (
     <>
       <div>
-        <Square value={squares[0]}></Square>
+        <Square value={squares[0]} onSquareClick={handleClick}></Square>
         <Square value={squares[1]}></Square>
         <Square value={squares[2]}></Square>
       </div>
